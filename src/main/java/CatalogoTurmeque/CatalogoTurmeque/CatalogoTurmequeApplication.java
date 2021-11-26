aff06318ed6c48f473ee18b95f5091bbe3c4a0e7
FrancisRoseroMT <93357157+FrancisRoseroMT@users.noreply.github.com>
2021-11-25 23:18:24 -0600

    Ciclo4

---
+++ src/main/java/CatalogoTurmeque/CatalogoTurmeque/CatalogoTurmequeApplication.java
@@ -0,0 +1,13 @@
+package CatalogoTurmeque.CatalogoTurmeque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoTurmequeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoTurmequeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CatalogoTurmeque/CatalogoTurmeque/CatalogoTurmequeApplicationTests.java
@@ -0,0 +1,13 @@
+package CatalogoTurmeque.CatalogoTurmeque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoTurmequeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
